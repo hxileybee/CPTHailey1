76642e5f6b17112f192f7e599f8eff2c59360784
hxileybee <hailey.boland26@ycdsbk12.ca>
2025-05-23 09:33:31 -0400

    Random Text

--- CPTHailey.java
+++ CPTHailey.java
@@ -4,5 +4,9 @@ public class CPTHailey{
 	public static void main(String[] args){
 		Console con = new Console();
 		con.println("Hello World");
+		con.println("My name is Hailey");
+		con.print("How old am I?");
+		con.println(" 16 (almost 17!!)");
+		con.println("Im excited for the semester to be over");
 	}
 }
